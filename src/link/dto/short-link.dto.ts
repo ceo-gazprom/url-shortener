41ac63963f656bc7a203e0d7db3028728936c296
Вакер Юрий <iyuvaker@sbercloud.ru>
2021-04-05 01:10:06 +0300

    RM: short link dto

--- src/link/dto/short-link.dto.ts
+++
@@ -1,6 +0,0 @@
-// import { IsUrl, IsString, MinLength, MaxLength, Length } from 'class-validator';
-// import { ApiProperty } from '@nestjs/swagger';
-
-// @ApiProperty()
-// @IsString()
-// @Length(value, 6)
