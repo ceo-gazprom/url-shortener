17b7c8156743e3a747fa7471ae4bd904d35fd581
Вакер Юрий <iyuvaker@sbercloud.ru>
2021-04-05 19:23:48 +0300

    UPD: Redirect, error handlers

--- src/link/link.controller.ts
+++ src/link/link.controller.ts
@@ -2,64 +2,64 @@ import {
   Controller,
   Get,
   Post,
-  Res,
   Param,
   Body,
+  Redirect,
   HttpException,
   HttpStatus,
 } from '@nestjs/common';
-import { ApiMovedPermanentlyResponse, ApiResponse } from '@nestjs/swagger';
-import { Response } from 'express';
+import { ApiResponse } from '@nestjs/swagger';
 import { LinkService } from './link.service';
 import { LongLinkDto } from './dto/long-link.dto';
-import { ErrorResponse } from './classes/response.class';
 
 @Controller()
 export class LinkController {
   constructor(private readonly linkService: LinkService) {}
 
   @Get(':urlCode')
-  @ApiMovedPermanentlyResponse({ status: 200, description: 'Redirect on url.' })
+  @Redirect()
+  @ApiResponse({ status: 302, description: 'Redirect on url.' })
   @ApiResponse({
     status: 404,
     description: 'Exception: not exist short link.',
-    type: ErrorResponse,
   })
   @ApiResponse({
     status: 400,
     description: 'Exception: bad short link.',
-    type: ErrorResponse,
   })
-  async redirect(@Param('urlCode') shortLink: string, @Res() res: Response) {
+  async redirect(@Param('urlCode') shortLink: string) {
     /** Validate incoming param */
     const isValidate = this.linkService.validateShortLink(shortLink);
 
-    if (!isValidate) {
+    if (!isValidate)
       throw new HttpException(
-        {
-          status: 'error',
-          error: 'Exception: bad short link.',
-        },
+        'Exception: bad short link.',
         HttpStatus.BAD_REQUEST,
       );
-    }
+
     const url = await this.linkService.getLongByShort(shortLink);
 
-    if (!url) {
+    if (!url)
       throw new HttpException(
-        {
-          status: 'error',
-          error: 'Exception: not exist short link.',
-        },
+        'Exception: not exist short link.',
         HttpStatus.NOT_FOUND,
       );
-    }
-    return res.redirect(url);
+
+    return {
+      url: url,
+      statusCode: HttpStatus.FOUND,
+    };
   }
 
   @Post('create')
   async createShortLink(@Body() body: LongLinkDto) {
     const shortUrl = await this.linkService.createShort(body.longLink);
-    return { status: 'success', shortLink: shortUrl };
+    if (!shortUrl)
+      throw new HttpException(
+        'Exception: invalid link.',
+        HttpStatus.BAD_REQUEST,
+      );
+
+    return { statusCode: HttpStatus.OK, shortLink: shortUrl };
   }
 }
